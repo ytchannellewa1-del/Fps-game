42bf0e0e3e423e8e3b33250e76f58180efdd88b6
ytchannellewa1-del <ytchannellewa1@gmail.com>
2026-01-22 11:14:05 +0100

    Define weapon properties in weapons.js
    Added weapon statistics for shotgun, SMG, AR, and sniper.

---
+++ weapons.js
@@ -0,0 +1,27 @@
+export const weapons = {
+  shotgun: {
+    pellets: 8,
+    optimalRange: 5,
+    maxRange: 12,
+    maxDamage: 100,
+    minDamage: 5,
+    spread: 0.18,
+    knockback: 0.35
+  },
+  smg: {
+    optimalRange: 15,
+    maxRange: 30,
+    damage: 18
+  },
+  ar: {
+    optimalRange: 30,
+    maxRange: 60,
+    damage: 30
+  },
+  sniper: {
+    optimalRange: 100,
+    maxRange: 200,
+    headshot: 100,
+    body: 80
+  }
+};
